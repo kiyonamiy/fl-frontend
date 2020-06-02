c1efa784b6b22d2694364f8c861585889f9f6c95
KiyonamiYu <514723273@qq.com>
2020-06-02 10:33:42 +0800

    fix: space多写导致重影

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,15 +5,13 @@ import '../assets/css/frame.css';
 import Client from './client-1';
 import ModelInfo from './model-info';
 import React, { useEffect } from 'react';
-import '../assets/css/frame.css'
+import '../assets/css/frame.css';
 import BoxPlot from './client/box-plot';
 import ServerClient from './client/server-client';
 import { BarChartPane } from './client/bar-chart';
 import { SpacePane } from './space';
 import { UtilsPane } from './utils';
 import { GradientPane } from './gradient';
-
-
 
 export default function AppPane(): JSX.Element {
   return (
@@ -34,10 +32,9 @@ export default function AppPane(): JSX.Element {
         <ModelInfo />
       </div>
       <div className="Frame GradientView">Gradient</div>
-      <SpacePane></SpacePane>
-      <GradientPane></GradientPane>
-      <div className='Frame ModelView'>Model</div>
-      <UtilsPane></UtilsPane>
+      <GradientPane />
+      <div className="Frame ModelView">Model</div>
+      <UtilsPane />
     </div>
   );
 }
